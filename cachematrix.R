f0387d4e91f455c69b4008b332dbf74b1d7a3fd2
Nick Donias <ndonias@users.noreply.github.com>
2018-06-11 18:13:53 -0400

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
+## The overall goal of my two functions is to cahce a result in order to save
+## save computing time for finding the inverse of a matrix.
+## The first funciton assigns the variables and creates the cache.
+## The second variable first checks to see if the calculation has been cached, if not
+## it performs the calculation.
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
 }
